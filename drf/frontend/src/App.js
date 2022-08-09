e62e40a1c89121a550e6a760080ebb18027cdfef
kodzyz <kodzyzdok@gmail.com>
2022-08-09 21:03:57 +0300

    frontend one way data flow

--- drf/frontend/src/App.js
+++ drf/frontend/src/App.js
@@ -1,25 +1,67 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ClientList from './components/Client.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            'authors': []
+        }
+    }
+
+    componentDidMount(){
+        const authors = [
+            {
+                 "username": "root",
+                 "first_name": "Константин",
+                 "last_name": "Давидюк",
+                 "age": 42,
+                 "email": "root@mail.ru",
+                 "cat": 1
+            },
+            {
+                    "username": "demn",
+                    "first_name": "Дмитрий",
+                    "last_name": "Егоров",
+                    "age": 45,
+                    "email": "yegorov@mail.ru",
+                    "cat": 2
+            },
+            {
+                    "username": "true",
+                    "first_name": "Андрей",
+                    "last_name": "Трусов",
+                    "age": 36,
+                    "email": "trusov@mail.ru",
+                    "cat": 3
+            },
+            {
+                    "username": "Anastasi",
+                    "first_name": "",
+                    "last_name": "",
+                    "age": null,
+                    "email": "nasta@mail.ru",
+                    "cat": 2
+            },
+        ]
+        this.setState(
+            {
+                'authors': authors
+            }
+        )
+
+    }
+
+    render(){
+        return(
+            <div>
+                <ClientList clients={this.state.authors} />
+            </div>
+        )
+    }
 }
 
 export default App;
