2d2bd2f05026e912d0b8f0fd7246f37d052ab38a
kodzyz <kodzyzdok@gmail.com>
2022-08-14 01:55:07 +0300

    API:models:Project,TODO(post/get)

--- drf/client/admin.py
+++ drf/client/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from client.models import *
 
-admin.site.register(ClientUser)
-admin.site.register(Category)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'age', 'avatar', 'email', 'cat')
+    list_display_links = ('id', 'email')
+    search_fields = ('username', 'email')
+
+
+class CatAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+
+admin.site.register(ClientUser, UserAdmin)
+admin.site.register(Category, CatAdmin)
