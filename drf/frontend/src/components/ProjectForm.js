b5a141d98c6dad2a59b5bb49b8ba7453ba87afb7
kodzyz <kodzyzdok@gmail.com>
2022-09-13 22:00:22 +0300

    handleProjectSelect console.log(Project)

--- drf/frontend/src/components/ProjectForm.js
+++ drf/frontend/src/components/ProjectForm.js
@@ -18,8 +18,28 @@ class ProjectForm extends React.Component {
         })
     }
 
+    handleProjectSelect(event) {
+        if (!event.target.selectedOptions) {
+            this.state({
+                'user': []
+            })
+            return;
+        }
+
+        let user = []
+
+        for(let option of event.target.selectedOptions) {
+            user.push(option.value)
+        }
+
+        this.setState({
+            'user': user
+        })
+    }
+
     handleSubmit(event) {
         //this.props.obtainAuthToken(this.state.login, this.state.password)
+        console.log(this.state.name, this.state.repo, this.state.user)
         event.preventDefault()
     }
 
@@ -29,7 +49,7 @@ class ProjectForm extends React.Component {
                 <form onSubmit={(event) => this.handleSubmit(event)}>
                     <input type="text" name="name" placeholder="name" value={this.state.name} onChange={(event) => this.handleChange(event)} />
                     <input type="text" name="repo" placeholder="repo" value={this.state.repo} onChange={(event) => this.handleChange(event)} />
-                    <select multiple>
+                    <select multiple onChange={(event) => this.handleProjectSelect(event)}>
                         {this.props.clients.map((client) => <option value={client.id}>{client.email}</option> )}
                     </select>
                     <input type="submit" value="Create" />
